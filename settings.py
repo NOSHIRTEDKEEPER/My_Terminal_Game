fa5c0ce61e467923cfb4082ee6b1fc5d8292f3d0
NOSHIRTEDKEEPER <117466542+NOSHIRTEDKEEPER@users.noreply.github.com>
2022-11-16 14:08:44 -0500

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,29 @@
+LEVEL_MAP = [
+'                                      																  x											',
+'                         XX           														    X											X		',
+'                                      												       X													X		',
+'       XXXX           XX              																	     X								X		',
+'   P                                  											     X														X		',
+'XXXXX         XX         XX           																		    X							X		',
+' XXXX       XX                        						XX				   XX					xx										X		',
+' XX    X  XXXX    XX  XX              					XX		XX												    X						X		',
+'       X  XXXX    XX  XXX             		XXXXXXXXXX					XX																X		',
+'    XXXX  XXXXXX  XX  XXXX  XX      XX 																			    XXxxxxxxxxxxxx		',
+'XXXXXXXX  XXXXXX  XX  XXXX       XX    																										']
+
+TILE_SIZE = 64
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+
+# colors 
+BG_COLOR = '#060C17'
+PLAYER_COLOR = '#C4F7FF'
+TILE_COLOR = '#94D7F2'
+
+# camera
+CAMERA_BORDERS = {
+	'left': 100,
+	'right': 200,
+	'top':100,
+	'bottom': 150
+}
